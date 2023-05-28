e6abd0b642f8e6b38321c30db469d2f46a07ca4b
Andrew-9810 <Andrew-9810@yandex.ru>
2023-05-28 11:40:52 +0300

    add model Group, Follow

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,7 +4,18 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    """Модель Group для сообществ."""
+    title = models.CharField('Имя', max_length=200)
+    slug = models.SlugField('Адрес', max_length=255, unique=True)
+    description = models.TextField('Описание', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
+    """Модель Post для хранения постов"""
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
@@ -17,6 +28,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
+    """Модель Comment для хранения комментариев"""
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='comments')
     post = models.ForeignKey(
@@ -24,3 +36,24 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Follow (models.Model):
+    """Модель Follow подписки на автора"""
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower'
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'following'], name='user_following'
+            )
+        ]
